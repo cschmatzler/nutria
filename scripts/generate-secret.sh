5b31567203ba6fa2ef6ade987dca4c4e68f09827
Christoph Schmatzler <christoph@schmatzler.dev>
2022-10-09 20:15:06 +0200

    fix secret generation with support for spaces

--- scripts/generate-secret.sh
+++ scripts/generate-secret.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 set -eo pipefail
+set -a
 
+source $1.env.secret
 sops --age $AGE_PUBLIC_KEY --input-type dotenv -e $1.env.secret > $1.env.enc
-env $(grep -v '^#' $1.env.secret | xargs -0) gomplate -f $1.tpl | sops --age $AGE_PUBLIC_KEY --input-type yaml --encrypted-regex '^(data|stringData)$' -e /dev/stdin > $1.enc
+gomplate -f $1.tpl | sops --age $AGE_PUBLIC_KEY --input-type yaml --encrypted-regex '^(data|stringData)$' -e /dev/stdin > $1.enc
