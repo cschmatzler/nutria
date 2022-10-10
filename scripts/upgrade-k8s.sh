3edf91fc337f0fdc544bd50d89950c98d5367cd2
Christoph Schmatzler <christoph@schmatzler.dev>
2022-10-10 23:31:14 +0200

    Fix script version numbers

--- scripts/upgrade-k8s.sh
+++ scripts/upgrade-k8s.sh
@@ -2,10 +2,10 @@
 
 set -eo pipefail
 
-yq '.machine.kubelet.image = "ghcr.io/siderolabs/kubelet:'$1'"' -i talos/controlplane.yaml
-yq '.cluster.apiServer.image = "k8s.gcr.io/kube-apiserver:'$1'"' -i talos/controlplane.yaml
-yq '.cluster.controllerManager.image = "k8s.gcr.io/kube-controller-manager:'$1'"' -i talos/controlplane.yaml
-yq '.cluster.proxy.image = "k8s.gcr.io/kube-proxy:'$1'"' -i talos/controlplane.yaml
-yq '.cluster.scheduler.image = "k8s.gcr.io/kube-scheduler:'$1'"' -i talos/controlplane.yaml
+yq '.machine.kubelet.image = "ghcr.io/siderolabs/kubelet:v'$1'"' -i talos/controlplane.yaml
+yq '.cluster.apiServer.image = "k8s.gcr.io/kube-apiserver:v'$1'"' -i talos/controlplane.yaml
+yq '.cluster.controllerManager.image = "k8s.gcr.io/kube-controller-manager:v'$1'"' -i talos/controlplane.yaml
+yq '.cluster.proxy.image = "k8s.gcr.io/kube-proxy:v'$1'"' -i talos/controlplane.yaml
+yq '.cluster.scheduler.image = "k8s.gcr.io/kube-scheduler:v'$1'"' -i talos/controlplane.yaml
 
-yq '.machine.kubelet.image = "ghcr.io/siderolabs/kubelet:'$1'"' -i talos/worker.yaml
+yq '.machine.kubelet.image = "ghcr.io/siderolabs/kubelet:v'$1'"' -i talos/worker.yaml
